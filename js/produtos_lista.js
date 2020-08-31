7b32e6d551488e955bd99ba55601f6cc6531893b
marcello_benigno <benigno.marcello@gmail.com>
2020-08-31 16:49:31 -0300

    [Vue] Diretivas v-for e v-bind closes #11

---
+++ js/produtos_lista.js
@@ -0,0 +1,30 @@
+const produtosLista = [
+    {
+        id: 1,
+        nome: 'JSRaiz para FW',
+        preco: 336,
+        descricao: 'O melhor curso de JS',
+        imagem: 'http://picsum.photos/501'
+    },
+    {
+        id: 2,
+        nome: 'WTDD',
+        preco: 400,
+        descricao: 'Aprenda Django',
+        imagem: 'http://picsum.photos/502'
+    },
+    {
+        id: 3,
+        nome: 'Python Birds',
+        preco: 530,
+        descricao: 'OO com Python',
+        imagem: 'http://picsum.photos/503'
+    },
+    {
+        id: 4,
+        nome: 'Laravel - Caelum',
+        preco: 560,
+        descricao: 'PHP do jeito certo',
+        imagem: 'http://picsum.photos/504'
+    },
+];
